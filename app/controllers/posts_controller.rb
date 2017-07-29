a0c4e9114c7f046e55fa7557b634028929e731e0
SegueX <free1989xj@hotmail.com>
2017-07-29 19:54:27 +0800

    implement post function & devise

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,55 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, :except => [:index, :show]
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      flash[:notice] = "Create successed "
+      redirect_to posts_path(@post)
+    else
+      flash[:alert] = "Create failed"
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    if @post.save
+      flash[:notice] = "Edit successed "
+      redirect_to posts_path(@post)
+    else
+      flash[:alert] = "Edit failed"
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:alert] = "Delete successed"
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+end

027bea8e92cc9bf60ea6453ce5c6041fc4f36771
SegueX <free1989xj@hotmail.com>
2017-10-21 22:22:25 +0800

    add rspec to project

--- spec/features/comment_spec.rb
+++ spec/features/comment_spec.rb
@@ -1,31 +1,31 @@
 require 'rails_helper'
 
-feature "post meetup", :type => :feature do
+feature "post comment", :type => :feature do
   before do
     @user = User.create!( :email => "Segue@gmail.com", :password => "123456")
     @post = Post.create!( :title => "chengdu meetup", :content => "blablablabla", :user => @user )
-  end
-  example "post failed" do
-    visit "/posts/new"
-    expect(page).to have_content("You need to sign in or sign up before continuing")
-  end
-
-  example "post successed" do
     visit "/posts/new"
     expect(page).to have_content("Log in")
-
     within("#new_user") do
       fill_in "Email Address", with: "Segue@gmail.com"
       fill_in "Password", with: "123456"
     end
     click_button "Log in"
     expect(page).to have_content("Signed in successfully")
+    visit "/posts/1"
+  end
 
-    within("#new_post") do
-      fill_in :post_title, with: "foo"
-      fill_in :'post[content]', with: "bar"   # 2 ways
+  example "comment failed" do
+    click_button "Submit"
+    expect(page).to have_content("Comment failed")
+  end
+
+  example "comment successed" do
+    within("#new_comment") do
+      fill_in :comment_content, with: "hello!"
     end
     click_button "Submit"
-    expect(page).to have_content("Create successed")
+    expect(page).to have_content("Comment successed")
   end
+
 end
